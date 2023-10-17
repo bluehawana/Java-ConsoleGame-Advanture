d73be87cf3ab086e2757a2295f96ed8b226928a4
Hongzhi Li <bluehawana@gmail.com>
2023-10-17 18:45:00 +0200

    fixade GameEngine.java

--- src/main/java/se/dsve/adventureawaits/GameEngine.java
+++ src/main/java/se/dsve/adventureawaits/GameEngine.java
@@ -16,28 +16,69 @@ public class GameEngine {
 
     // Getter for startHp
     public int getStartHp() {
+        if (startHp == 0) {
+            return 100;
+        } else {
+            return startHp;
+        }
+
         // Skriv din kod här
     }
 
     public void setDifficulty(int startHp, int levelXp, int weaponDamage) {
         // Skriv din kod här
         // Om spelet inte har startat än, så kan du ändra svårighetsgraden
+        if (!isGameStarted()) {
+            this.startHp = startHp;
+            this.levelXp = levelXp;
+            this.weaponDamage = weaponDamage;
+        }
+        else {
+            System.out.println("Du kan inte byta difficulty när spelet har startat.");
+        }
+
     }
 
     public void startGame() {
         // Skriv din kod här
         // Start the game
+        gameStarted = true;
+        currentPlayerHp = getStartHp();
+        System.out.println("Spelet har startat.");
     }
 
     public void endGame() {
         // End the game
+        gameStarted = false;
+        System.out.println("Spelet har avslutats.");
     }
 
     public void gameLoop() {
         // Main game loop
-    }
+        if (isGameStarted()) {
+            if (isPlayerAlive()) {
+                // Skriv din kod här
+                System.out.println("Du har " + currentPlayerHp + " hp kvar.");
+            } else {
+                // Skriv din kod här
+System.out.println("Du har dött.");
+endGame();
+            } } else {
+                System.out.println("Du måste starta spelet först.");
+
+
+
+
+
+
+            }
+            // Skriv din kod här
+        }
+
 
     public boolean isGameStarted() {
         // Skriv din kod här
+        return gameStarted;
     }
-}
+
+    }
